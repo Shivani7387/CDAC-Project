0a50b9cbc9c3324e390990e4f85e54aa044461b6
Rahul Dethe <81143446+mederamusic@users.noreply.github.com>
2023-08-29 00:27:10 +0530

    bm_xCAT.sh

--- VM or BM/bm_xCAT.sh
+++ VM or BM/bm_xCAT.sh
@@ -59,3 +59,45 @@ source /etc/profile.d/xcat.sh
 #YOU HAVE TO MANUALLY SOURCE  /etc/profile.d/xcat.sh
 #hostname_master=$(zenity --entry --title="source /etc/profile.d/xcat.sh" --text="xCAT has #been installed successfully, type the command shown on the title on the terminal to unlock xCAT.")
 whiptail --msgbox "xCAT has been installed successfully, Press OK to continue." 10 50
+
+
+#PART 3
+
+
+domain_name=`cat /root/inputs/domain_name`
+hostname_master=`cat /root/inputs/hostname_master`
+private_interface=`cat /root/inputs/private_interface`
+private_ip=`cat /root/inputs/private_ip`
+
+chdef -t site domain=$domain_name
+dhcpinterfaces="chdef -t site dhcpinterfaces='$hostname_master|$private_interface'"
+eval "$dhcpinterfaces"
+worker_password=$(zenity --entry --title="Password" --text="Provide a common password for all worker nodes : ")
+#read -p "Provide Password for worker node : " worker_password
+passwd="chtab key=system passwd.username=root passwd.password=$worker_password"
+eval "$passwd"
+#chtab key=system passwd.username=root passwd.password=$worker_password
+makedhcp -n
+makedns -n
+makentp
+systemctl restart dhcpd
+
+
+#PART 4
+
+
+wget http://mirrors.nxtgen.com/centos-mirror/7.9.2009/isos/x86_64/CentOS-7-x86_64-DVD-2009.iso
+copycds CentOS-7-x86_64-DVD-2009.iso
+genimage centos7.9-x86_64-netboot-compute
+
+touch /install/netboot/compute.synclists
+echo "/etc/passwd -> /etc/passwd" >> /install/netboot/compute.synclist
+echo "/etc/group -> /etc/group" >> /install/netboot/compute.synclist
+echo "/etc/shadow -> /etc/shadow" >> /install/netboot/compute.synclist
+
+path_of_computelist="\"/install/netboot/compute.synclists\""
+compute_list="chdef -t osimage centos7.9-x86_64-netboot-compute synclists=$path_of_computelist"
+eval "$compute_list"
+
+chdef -t osimage centos7.9-x86_64-netboot-compute synclists="/install/netboot/compute.synclists" #This does not run in bash, run this command manually on terminal
+#packimage centos7.9-x86_64-netboot-compute
